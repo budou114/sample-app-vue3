18be1df8dfcedf88fe73159f5e344280218029eb
budou114 <58349030+budou114@users.noreply.github.com>
2021-11-07 13:02:26 +0900

    遷移先画面の仮作成

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,20 +2,67 @@ import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import Top from "../views/Top.vue";
 
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: "/",
-    name: "Top",
-    component: Top,
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
-  },
+	{
+		path: "/",
+		name: "Top",
+		component: Top,
+		meta: {
+		title: "Topページ",
+		description: "Topページの説明"
+		}
+	},
+	{
+		path: "/about",
+		name: "About",
+		component: () =>
+		import(
+			/* webpackChunkName: "about" */
+			"../views/About.vue"
+		),
+		meta: {
+		title: "Aboutページ",
+		description: "Aboutページの説明"
+		}
+	},
+	{
+		path: "/author",
+		name: "Author",
+		component: () =>
+		import(
+			/* webpackChunkName: "author" */
+			"../views/Author.vue"
+		),
+		meta: {
+		title: "Authorページ",
+		description: "Authorページの説明"
+		}
+	},
+	{
+		path: "/production-note",
+		name: "Production note",
+		component: () =>
+		import(
+			/* webpackChunkName: "production-note" */
+			"../views/ProductionNote.vue"
+		),
+		meta: {
+		title: "ProductionNoteページ",
+		description: "ProductionNoterページの説明"
+		}
+	},
+	{
+		path: "/faq",
+		name: "Q&A",
+		component: () =>
+		import(
+			/* webpackChunkName: "faq" */
+			"../views/Faq.vue"
+		),
+		meta: {
+		title: "Faqページ",
+		description: "Faqページの説明"
+		}
+	},
 ];
 
 const router = createRouter({
